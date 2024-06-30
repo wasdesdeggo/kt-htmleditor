d961efda61c85dd8b647a7e1ce4424de600e8f8a
Krits431 <122638098+wasdesdeggo@users.noreply.github.com>
2024-06-30 11:07:44 +0800

    Update autoyoutube.js

--- plugins/autoyoutube.js
+++ plugins/autoyoutube.js
@@ -2,7 +2,7 @@
  * Auto Youtube Plugin
  * 
  *
- * Copyright (C) 2016, 
+ * Copyright (C) 2016, wasdesdeggo
  *
  *  licensed under the MIT license:
  *	http://www.opensource.org/licenses/mit-license.php
